ced9f4a3660df3bab10bc7b24ac1f9603d0cdc83
Todd Tarsi <ttarsi@eplata.com>
2019-07-24 17:03:51 -0500

    fix(transport): conveniently nest options here

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+import { DogapiTransport } from './transports/dogapi-transport';
+import { DogapiTransportOptions } from './transports/dogapi-transport-options';
+
 export { Logger } from './logger/logger';
 export { WinstonEvent } from './events/winston-event.enum';
-export { DogapiTransport as Transport } from './transports/dogapi-transport';
+
+export const Transport = (options: DogapiTransportOptions) => new DogapiTransport({ dogapiTransportOptions: options });
