dc35bde7aecfeddf6f3ac7d1c27b4e993c668ea9
kioannou <kostazj@gmail.com>
2019-08-06 20:31:54 +0300

    applied format in dogapi-transport-standalone.ts

--- src/transports/dogapi-transport-standalone.ts
+++ src/transports/dogapi-transport-standalone.ts
@@ -5,7 +5,8 @@ import { DogapiTransportOptions } from './dogapi-transport-options';
 export const DogapiTransportStandalone = (
   ddOptions: DogapiTransportOptions,
   options: LoggerOptions = new LoggerOptions(),
-) => new DogapiTransport({
-  ...options,
-  dogapiTransportOptions: ddOptions,
-});
+) =>
+  new DogapiTransport({
+    ...options,
+    dogapiTransportOptions: ddOptions,
+  });
